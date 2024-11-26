138b1a141c1da081c8dff3f642aaa314a96754c5
Ayushi Anand <160775991+Or4cle404@users.noreply.github.com>
2024-11-26 19:23:04 +0530

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const videoElement = document.getElementById('videoFeed');
+    
+    setInterval(() => {
+       
+        console.log('Sending gesture data to backend...');
+    }, 1000); // Update every second (adjust based on need)
+};
